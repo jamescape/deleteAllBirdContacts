bbc6090988b1615cb18d572288113c6a5d44350a
James Barber <james@jamesbarber.tech>
2025-07-18 12:53:14 +0100

    Removed filter

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const res = await fetch('https://api.bird.com/workspaces/' + process.env.WORKSPA
 	})
 let currentContacts = await res.json()
 contacts = contacts.concat(currentContacts.results)
+console.log('Initial contacts fetched: ' + contacts.length)
 
 while (currentContacts.nextPageToken != null) {
 	const res = await fetch(`https://api.bird.com/workspaces/${process.env.WORKSPACE_ID}/contacts?limit=100&pageToken=${currentContacts.nextPageToken}`, {
@@ -17,17 +18,18 @@ while (currentContacts.nextPageToken != null) {
 
  	currentContacts = await res.json()
 	contacts = contacts.concat(currentContacts.results)
+	console.log('Fetched more contacts (' + currentContacts.results.length + '), total now: ' + contacts.length)
 }
 
+console.log('Total contacts fetched: ' + contacts.length)
+
 Promise.all(contacts.map(async contact => {
-	if (contact.featuredIdentifiers[0].value.includes('tu3')) {
-		console.log('Deleting contact: ' + contact.featuredIdentifiers[0].value)
-		const res = await fetch(`https://api.bird.com/workspaces/${process.env.WORKSPACE_ID}/contacts/${contact.id}`, {
-			method: 'DELETE',
-			headers: new Headers({
-				'Authorization': 'AccessKey ' + process.env.BIRD_KEY,
-			})
+	console.log('Deleting contact: ' + contact.featuredIdentifiers[0].value)
+	const res = await fetch(`https://api.bird.com/workspaces/${process.env.WORKSPACE_ID}/contacts/${contact.id}`, {
+		method: 'DELETE',
+		headers: new Headers({
+			'Authorization': 'AccessKey ' + process.env.BIRD_KEY,
 		})
-		return res.ok
-	}
+	})
+	return res.ok
 }))
